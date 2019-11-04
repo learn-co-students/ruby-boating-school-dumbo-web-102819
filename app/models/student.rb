9d51249d49ba342bb71be7a87a0557f73501854a
Dominic Chu <dominic.m.chu@gmail.com>
2019-11-03 22:38:56 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
 class Student
+    @@all = []
+    attr_reader :first_name
+    def initialize(first_name)
+        @first_name = first_name 
+        @@all << self
+    end 
+    def self.all
+        @@all
+    end 
+    def self.find_student(first_name)
+        @@all.find do |instance|
+            instance.first_name == first_name 
+        end 
+    end 
+    def add_boating_test(test_name, status, instructor)
+        BoatingTest.new(test_name, status, self, instructor)
+    end 
+    def boating_tests 
+        BoatingTest.all.select do |instance|
+            instance.student == self
+        end 
+    end 
 
+    def grade_percentage
+        count = 0
+       boating_tests.each do |instance|
+        if instance.status == "passed"
+            count += 1
+        end 
+       end 
+       count * 1.00 / boating_tests.count
+    end 
 end
