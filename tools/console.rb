60ddae8a43ed6726fb4dd9cf31f85c742e69185b
ozubergz <jmin499@gmail.com>
2019-11-05 03:34:20 -0500

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,9 +4,25 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+#create new student instance
+spongebob = Student.new("Spongebob")
+patrick= Student.new("Patrick")
+
+#create new instructor instance
+puff= Instructor.new("Ms.Puff")
+krabs= Instructor.new("Mr.Krabs")
+
+#create new boating test instance
+boat_test1 = BoatingTest.new(spongebob, "Don't Crash 102", "passed", puff)
+
+#add new boating test
+no_crashing = spongebob.add_boating_test("Don't Crash 101", "pending", puff)
+power_steering_failure = patrick.add_boating_test("Power Steering 202", "failed", puff)
+power_steering_pass = patrick.add_boating_test("Power Steering 201", "passed", krabs)
+
+#pass student
+# puff.pass_student(patrick, "Power Steering 202")
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
