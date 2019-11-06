9dbf919fe8c9434437ac7c9e267676ff30c92f62
Frank Gerold <FrankETSGerold@gmail.com>
2019-11-06 09:08:53 -0500

    Added deliverables to each class file, added example seed data to console.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,8 @@
+#
+# `BoatingTest` class:
+# * should initialize with Student (Object), a boating test name (String), a boating test status (String), and an Instructor (Object)
+# * `BoatingTest.all` returns an array of all boating tests
+
 class BoatingTest
 
 end
